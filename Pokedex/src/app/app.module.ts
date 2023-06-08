09d693e1d6daa5997cd884655b8b963e58485bf6
FranAchard <franachard8@gmail.com>
2023-06-08 15:05:44 -0300

    Header y Footer

--- Pokedex/src/app/app.module.ts
+++ Pokedex/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './page/home/home.component';
+import { FotoPokemonComponent } from './components/foto-pokemon/foto-pokemon.component';
+import { TarjetaPokemonComponent } from './components/tarjeta-pokemon/tarjeta-pokemon.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    FotoPokemonComponent,
+    TarjetaPokemonComponent
   ],
   imports: [
     BrowserModule,
